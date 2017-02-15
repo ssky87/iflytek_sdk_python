26f5d3b438526ae47c8a6e05c318dd5c5d2dcbd6
dhc <sky87@qq.com>
2017-02-15 17:34:57 +0800

    first commit

---
+++ stt.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*- 
+import time
+from ctypes import *
+from io import BytesIO
+import platform
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def play(filename):
+    import pygame
+    pygame.mixer.init(frequency=16000)
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
+
+
+
+def getWaveData(_tmpFile = 'iflytek01.wav'):
+    with open(_tmpFile,'rb') as f:
+        return f.read()
+
+
+def convDataToPointer(wav_data):
+    tmpBytes = BytesIO()
+    f_size = tmpBytes.write(wav_data)
+    array = ( c_byte * f_size)()
+    tmpBytes.seek(0,0)
+    tmpBytes.readinto(array)
+    return array
+
+
+def speech_to_text(waveData):
+    
+
+    plat = platform.architecture()
+    if plat[1] == 'WindowsPE' :
+        cur = cdll.LoadLibrary('./win/libmsc.lib')
+    else:
+        if plat[0] == '32bit':
+            cur = cdll.LoadLibrary('./x86/libmsc.so')
+        else:
+            cur = cdll.LoadLibrary('./x64/libmsc.so')
+       
+
+    MSPLogin = cur.MSPLogin
+    QISRSessionBegin = cur.QISRSessionBegin
+    QISRAudioWrite = cur.QISRAudioWrite
+    QISRGetResult = cur.QISRGetResult
+    QISRGetResult.restype = c_void_p
+    QISRSessionEnd = cur.QISRSessionEnd
+
+    p_pcm = convDataToPointer(waveData)
+    pcm_size = sizeof(p_pcm)
+    
+    ret_c = c_int(0)
+    ret = 0
+
+    ep_stat = c_int(0)
+    rec_stat = c_int(0)
+
+    ret = MSPLogin(None,None,'appid = 539feff8, work_dir = .') 
+    if ret != 0:
+        logging.error("MSPLogin failed, error code: " + ret);
+        return
+
+
+    logging.info("开始语音听写 ...")
+    session_begin_params = "sub = iat, domain = iat, language = zh_cn, accent = mandarin, sample_rate = 16000, result_type = plain, result_encoding = utf8";
+    sessionID = QISRSessionBegin(None,session_begin_params, byref(ret_c));
+    if ret_c.value != 0 :
+        logging.error("QISRSessionBegin failed, error code: " + ret_c.value);
+        return
+
+    pcm_count = 0
+    len = 10 * 640; #每次写入200ms音频(16k，16bit)：1帧音频20ms，10帧=200ms。16k采样率的16位音频，一帧的大小为640Byte
+    while True:
+        if pcm_size < 2 * len:
+            len = pcm_size
+        if len <= 0 :
+            break
+
+        aud_stat = 2
+        if 0 == pcm_count:
+            aud_stat = 1
+            
+        ret = QISRAudioWrite(sessionID, byref(p_pcm,pcm_count),len, aud_stat, byref(ep_stat), byref(rec_stat));
+        if ret != 0:
+            logging.error("QISRAudioWrite failed, error code: " + ret);
+            break
+
+        pcm_count += len
+        pcm_size  -= len
+
+        if ep_stat.value == 3:
+            break
+        time.sleep(.1)
+ 
+
+    ret = QISRAudioWrite(sessionID, None,0, 4, byref(ep_stat), byref(rec_stat));
+    if ret != 0:
+        logging.error("QISRAudioWrite failed, error code: " + ret);
+
+    error_c = c_int(0)
+    text_finish = ""
+    while  rec_stat.value != 5:
+        x = QISRGetResult(sessionID, byref(rec_stat), 0, error_c);
+        if x!=None:
+            num = 0
+            while True:
+                if (c_char).from_address(x+num).value != c_char('\x00').value:
+                    num += 1
+                else:
+                    break
+            text = (c_char * num).from_address(x)
+            text_finish += text.value
+
+        time.sleep(.1)
+    logging.debug(text_finish)
+    logging.info('合成完成！')
+    ret = QISRSessionEnd(sessionID, "Normal");
+    if ret != 0:
+        logging.error("QTTSTextPut failed, error code: " + ret);
+
+    return text_finish
+
+
+if __name__ == '__main__':
+    speech_to_text(getWaveData('xx.wav'))
---
+++ tts.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*- 
+import time
+from ctypes import *
+from io import BytesIO
+import wave
+import platform
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def play(filename):
+    import pygame
+    pygame.mixer.init(frequency=16000)
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
+
+
+
+def saveWave(raw_data,_tmpFile = 'test.wav'):
+    f = wave.open(_tmpFile,'w')
+    f.setparams((1, 2, 16000, 262720, 'NONE', 'not compressed'))
+    f.writeframesraw(raw_data)
+    f.close()
+    return _tmpFile
+
+def text_to_speech(src_text="这不仅仅是一个测试",file_name = None):
+
+    plat = platform.architecture()
+    if plat[1] == 'WindowsPE' :
+        cur = cdll.LoadLibrary('./win/libmsc.lib')
+    else:
+        if plat[0] == '32bit':
+            cur = cdll.LoadLibrary('./x86/libmsc.so')
+        else:
+            cur = cdll.LoadLibrary('./x64/libmsc.so')
+       
+
+    MSPLogin = cur.MSPLogin
+    QTTSSessionBegin = cur.QTTSSessionBegin
+    QTTSTextPut = cur.QTTSTextPut
+
+    QTTSAudioGet = cur.QTTSAudioGet
+    QTTSAudioGet.restype = c_void_p
+
+    QTTSSessionEnd = cur.QTTSSessionEnd
+    
+    ret_c = c_int(0)
+    ret = 0
+
+    ret = MSPLogin(None,None,'appid = 539feff8, work_dir = .') 
+    if ret != 0:
+        logging.error("MSPLogin failed, error code: " + ret);
+        return
+
+    session_begin_params="voice_name = xiaoyan, text_encoding = utf8, sample_rate = 16000, speed = 50, volume = 50, pitch = 50, rdn = 2"
+    sessionID = QTTSSessionBegin(session_begin_params, byref(ret_c));
+    if ret_c.value != 0 :
+        logging.error("QTTSSessionBegin failed, error code: " + ret_c.value);
+        return
+
+    ret = QTTSTextPut(sessionID, src_text, len(src_text),None)
+    if ret != 0:
+        logging.error("QTTSTextPut failed, error code: " + ret);
+        QTTSSessionEnd(sessionID, "TextPutError");
+        
+        return
+    logging.info("正在合成 [%s]..." %(src_text))
+
+    audio_len = c_uint(0)
+    synth_status = c_int(0)
+
+    f = BytesIO()
+    while True:
+        p = QTTSAudioGet(sessionID, byref(audio_len), byref(synth_status), byref(ret_c));
+        if ret_c.value != 0:
+            logging.error("QTTSAudioGet failed, error code: " + ret_c);
+            QTTSSessionEnd(sessionID, "AudioGetError");
+            break
+
+        if p != None:
+            buf = (c_char * audio_len.value).from_address(p)
+            f.write(buf)
+
+        if synth_status.value == 2 :
+            saveWave(f.getvalue(),file_name)
+            break
+
+        logging.debug(".")
+        time.sleep(1)
+
+    logging.info('合成完成！')
+    ret = QTTSSessionEnd(sessionID, "Normal");
+    if ret != 0:
+        logging.error("QTTSTextPut failed, error code: " + ret);
+
+
+if __name__ == '__main__':
+    text_to_speech('科大讯飞还是不错的','xx.wav')
+
